2db09ff689796d92530c565dfcade0d5a736b762
Nitin96Bisht <nitin.bisht96@gmail.com>
2019-07-07 12:26:43 +0530

    Added More Swagger Features

--- restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package com.example.rest.webservices.restfulwebservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,8 +17,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	private static final Contact DEFAULT_CONTACT = new Contact("Nitin Bisht", "http://localhost:8080", "nitin.bisht96@gmail.com");
+	
+	private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title", "Awesome API Description", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = 
+				new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 }
--- restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/example/rest/webservices/restfulwebservices/user/User.java
@@ -5,12 +5,18 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "All details about user.")
 public class User {
 
 	private Integer id;
 	@Size(min = 2, message = "Name should have at least 2 characters")
+	@ApiModelProperty(notes = "Name should have at least 2 characters")
 	private String name;
 	@Past
+	@ApiModelProperty(notes = "Birth date should be in the past")
 	private Date birthDate;
 	
 	public Integer getId() {
